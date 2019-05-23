ba5b18100a3cce0ff268d6115c3b70c07f1444ea
noobow34 <skywind.hk34@gmail.com>
2019-05-23 23:09:00 +0900

    コンストラクタ戻す

--- Noobow.Commons/EF/Tools/ToolsContext.cs
+++ Noobow.Commons/EF/Tools/ToolsContext.cs
@@ -17,7 +17,6 @@ namespace Noobow.Commons.EF
         public virtual DbSet<NotificationTask> NotificationTasks { get; set; }
 
         public ToolsContext(DbContextOptions<ToolsContext> options) : base(options) { }
-        public ToolsContext() { }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
