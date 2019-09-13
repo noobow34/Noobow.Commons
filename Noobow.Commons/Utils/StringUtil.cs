8ba4d4656908a9adeddbf5a931d6e7f7924a7259
noobow34 <skywind.hk34@gmail.com>
2019-09-13 21:13:58 +0900

    StringUtilを追加

---
+++ Noobow.Commons/Utils/StringUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Noobow.Commons.Utils
+{
+    public static class StringUtil
+    {
+        private static readonly string[] metaCharacters = new string[] { "^", "$", ".", "*", "+", "?", "(", ")", "{", "}", "[", "]", "^" };
+
+        public static bool ContainsAny(IEnumerable<string> ss,string s)
+        {
+            return ss.Any(c => s.Contains(c));
+        }
+
+        public static Boolean ContainsMetaCharacter(string s)
+        {
+            return ContainsAny(metaCharacters, s);
+        }
+    }
+}
