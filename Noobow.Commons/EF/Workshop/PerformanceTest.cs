d25d1f93cde517adeea5e3d554db12198d60fb35
noobow34 <skywind.hk34@gmail.com>
2019-09-15 15:04:16 +0900

    Workshop追加

---
+++ Noobow.Commons/EF/Workshop/PerformanceTest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Noobow.Commons.EF.Workshop
+{
+    [Table("PERFORMANCE_TEST")]
+    public class PerformanceTest
+    {
+        [Key]
+        [Column("KEY_COLUMN")]
+        public string KeyColumn { get; set; }
+        [Column("VALUE_COLUMN")]
+        public string ValueColumn { get; set; }
+    }
+}
---
+++ Noobow.Commons/EF/Workshop/WorkshopContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using EnumStringValues;
+using SiteUpdateChecker.Constants;
+using Noobow.Commons.EF.Workshop;
+using Noobow.Commons.Constants;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Noobow.Commons.EF
+{
+    public class WorkshopContext : DbContext
+    {
+
+        public virtual DbSet<CheckSite> CheckSites { get; set; }
+
+        public WorkshopContext(DbContextOptions<WorkshopContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                /*IServiceCollection serviceCollection = new ServiceCollection();
+                serviceCollection.AddLogging(builder => builder
+                .AddConsole()
+                .AddFilter(level => level >= LogLevel.Information)
+                );
+                var loggerFactory = serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();*/
+
+                var config = new ConfigurationBuilder().SetBasePath(System.AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appsettings.json").Build();
+                var connectionString = config.GetConnectionString("WorkshopConnection");
+                optionsBuilder./*UseLoggerFactory(loggerFactory).EnableSensitiveDataLogging().*/UseMySql(connectionString,
+                        mySqlOptions =>
+                        {
+                            mySqlOptions.ServerVersion(new Version(10, 3), ServerType.MariaDb);
+                        }
+                );
+            }
+        }
+    }
+}
