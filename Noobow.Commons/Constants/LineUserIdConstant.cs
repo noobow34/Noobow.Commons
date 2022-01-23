ab9d058679d97320287f7cba4091e17e9933df71
noobow34 <skywind.hk34@gmail.com>
2022-01-23 10:06:37 +0900

    ID追加

--- Noobow.Commons/Constants/LineUserIdConstant.cs
+++ Noobow.Commons/Constants/LineUserIdConstant.cs
@@ -7,5 +7,6 @@ namespace Noobow.Commons.Constants
     public static class LineUserIdConstant
     {
         public const string NOOBWO = "U68e05e69b6acbaaf565bc616fdef695d";
+        public const string HARUKAINANA = "U3afbf6ad375124c3e79c37dbacd481c2";
     }
 }
