a815106692a777a1f9dd792093001fa3f8558125
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-01-03 21:06:41 +0900

    通知先をSlackに変更

--- Noobow.Commons/Constants/EndpointConstant.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Noobow.Commons.Constants
-{
-    public static class EndpointConstant
-    {
-        public const string NOTIFY_ME_ENDPOINT = "http://localhost:4000/NotifyMe";
-    }
-}
---
+++ Noobow.Commons/Constants/SlackChannelEnum.cs
@@ -0,0 +1,18 @@
+﻿using EnumStringValues;
+
+namespace Noobow.Commons.Constants
+{
+    public enum SlackChannelEnum
+    {
+        [StringValue("C086R4A0KCP")]
+        amazon,
+        [StringValue("C086R2VNZTR")]
+        jafleet,
+        [StringValue("C0872P37T1T")]
+        megalos,
+        [StringValue("C087844ETGU")]
+        library,
+        [StringValue("C0873C925BP")]
+        SiteUpdate
+    }
+}
