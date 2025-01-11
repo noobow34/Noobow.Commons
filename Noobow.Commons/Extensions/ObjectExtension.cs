253fa855a6440b6e34c40378ff67ba1d0ac1d874
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-01-11 09:48:11 +0900

    インスタンスごとに実行IDを保持できるように修正

--- Noobow.Commons/Extensions/ObjectExtension.cs
+++ Noobow.Commons/Extensions/ObjectExtension.cs
@@ -1,19 +1,22 @@
 ﻿using Force.Crc32;
 using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Noobow.Commons.Extensions
 {
     public static class ObjectExtension
     {
-        private static string instanceId;
+        private static readonly ConditionalWeakTable<object, string> _instanceIdList = [];
+
         public static void JournalWriteLine(this object obj,string value)
         {
-            if (instanceId == null)
-            {
+            if (!_instanceIdList.TryGetValue(obj,out string instanceId)){
                 byte[] bytes = new UTF8Encoding().GetBytes(Guid.NewGuid().ToString());
                 uint crc32 = Crc32Algorithm.Compute(bytes);
                 instanceId = Convert.ToString(crc32, 16);
+                _instanceIdList.Add(obj,instanceId);
             }
             Console.WriteLine($"【{obj.GetType().Name}:{instanceId}】{value}");
         }
