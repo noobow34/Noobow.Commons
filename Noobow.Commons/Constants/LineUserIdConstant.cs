c918166c77e564ec68f733adcab20442ad6a96e9
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-01-31 22:05:13 +0900

    不要Enumを削除

--- Noobow.Commons/Constants/LineUserIdConstant.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Noobow.Commons.Constants
-{
-    public static class LineUserIdConstant
-    {
-        public const string NOOBWO = "U68e05e69b6acbaaf565bc616fdef695d";
-        public const string HARUKAINANA = "U3afbf6ad375124c3e79c37dbacd481c2";
-    }
-}
