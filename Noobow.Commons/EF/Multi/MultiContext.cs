05a03f9983640649a57ce3d25a0edd5b29da652e
noobow34 <skywind.hk34@gmail.com>
2026-02-16 23:46:45 +0900

    献血スタンプ外部化

--- Noobow.Commons/EF/Multi/MultiContext.cs
+++ Noobow.Commons/EF/Multi/MultiContext.cs
@@ -24,7 +24,6 @@ public partial class MultiContext : DbContext
     public virtual DbSet<BloodDonationUserInfo> BloodDonationUserInfos { get; set; }
     public virtual DbSet<BloodDonationCenter> BloodDonationCenters { get; set; }
     public virtual DbSet<BloodDonationReserveCheck> BloodDonationReserveChecks { get; set; }
-    public virtual DbSet<VisitStamp> VisitStamps { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         string connectionString = Environment.GetEnvironmentVariable("MULTI_CONNECTION_STRING") ?? "";
